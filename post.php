7b6ea8ce2f72bc41a8376348bee142e5da3213fa
Guilherme <goliva12@hotmail.com>
2024-10-02 15:56:38 -0300

    Finalização página interna dos posts

--- post.php
+++ post.php
@@ -11,62 +11,62 @@ if (isset($_GET['id'])) {
             $currentPost = $post;
         }
     }
-}
-?>
 
-<main class="post-container">
+}
+
+?>
+<main id="post-container">
     <div class="content-container">
         <h1 id="main-title"><?= $currentPost['title'] ?></h1>
         <p id="post-description"><?= $currentPost['description'] ?></p>
         <div class="img-container">
-            <img src="<?= $BASE_URL ?>img/<?= $post['img'] ?>" alt="<?= $post['title'] ?>">
+            <img src="<?= $BASE_URL ?>/img/<?= $currentPost['img'] ?>" alt="<?= $currentPost['title'] ?>">
         </div>
-        <p class="post-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non recusandae accusantium
-            reiciendis repudiandae. Deserunt, in laborum perspiciatis obcaecati totam quaerat, veniam eligendi maiores
-            doloremque, fuga sequi dignissimos reprehenderit optio placeat.
-            Aliquam ratione magnam iste! Dolorum, repellendus blanditiis. Natus, vitae nisi! Voluptatum ea alias neque
-            voluptate perferendis cupiditate repellendus eaque suscipit repudiandae, nisi id vero magni corporis odit
-            molestias numquam fuga!
-            Tempora nam dolores, minus, sit voluptatem dolorem molestiae, ex tempore cupiditate repellendus quae nobis
-            in incidunt? Aut sequi molestiae accusantium rem tempora doloribus quasi. Cumque consequatur quia excepturi
-            maiores in.
-            Sapiente dignissimos porro ratione eos magni explicabo velit illo, rerum expedita commodi repudiandae
-            temporibus sunt ab fugit, omnis, reprehenderit tenetur corporis? Quibusdam cum inventore modi ex harum. Nam,
-            iste eligendi.
-            Accusamus assumenda, ipsa aperiam temporibus odit iusto recusandae omnis, reprehenderit repudiandae repellat
-            unde, sunt rerum maxime amet quae nam error vitae sit doloribus autem facere corporis. Iste aperiam vero
-            cumque.</p>
+        <p class="post-content">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo, explicabo provident,
+            voluptatum, veniam nihil repellat eveniet quae adipisci exercitationem quos minus corrupti placeat veritatis
+            architecto excepturi laudantium nulla tenetur cupiditate.
+            Odit saepe voluptas voluptates, iusto minima dolore deleniti corporis itaque, rem facere inventore in sed
+            cumque voluptatibus unde! Assumenda nam aspernatur eveniet id illo inventore ratione laboriosam iusto culpa
+            provident.
+            Nam blanditiis autem fugiat officiis animi, adipisci consequuntur minima. Mollitia atque iste sapiente quod
+            pariatur necessitatibus minus voluptatem rerum eos modi enim perspiciatis provident ducimus, iure nostrum
+            nobis eum iusto.
+            Perspiciatis sed numquam animi quae assumenda nesciunt voluptatibus rem! Deserunt, quae ex, pariatur nemo
+            eveniet ipsam delectus aperiam aut quas blanditiis repellat quibusdam debitis sequi odit nostrum? Dolores,
+            iure quam!
+            Beatae vel corrupti laborum repudiandae, placeat neque officiis odit provident reiciendis ducimus amet rem.
+            Exercitationem similique itaque labore asperiores quasi officia aspernatur quas soluta quos. Quasi aliquam
+            in laboriosam illum.</p>
+        <p class="post-content">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo, explicabo provident,
+            voluptatum, veniam nihil repellat eveniet quae adipisci exercitationem quos minus corrupti placeat veritatis
+            architecto excepturi laudantium nulla tenetur cupiditate.
+            Odit saepe voluptas voluptates, iusto minima dolore deleniti corporis itaque, rem facere inventore in sed
+            cumque voluptatibus unde! Assumenda nam aspernatur eveniet id illo inventore ratione laboriosam iusto culpa
+            provident.
+            Nam blanditiis autem fugiat officiis animi, adipisci consequuntur minima. Mollitia atque iste sapiente quod
+            pariatur necessitatibus minus voluptatem rerum eos modi enim perspiciatis provident ducimus, iure nostrum
+            nobis eum iusto.
+            Perspiciatis sed numquam animi quae assumenda nesciunt voluptatibus rem! Deserunt, quae ex, pariatur nemo
+            eveniet ipsam delectus aperiam aut quas blanditiis repellat quibusdam debitis sequi odit nostrum? Dolores,
+            iure quam!
+            Beatae vel corrupti laborum repudiandae, placeat neque officiis odit provident reiciendis ducimus amet rem.
+            Exercitationem similique itaque labore asperiores quasi officia aspernatur quas soluta quos. Quasi aliquam
+            in laboriosam illum.</p>
     </div>
-    <p class="post-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non recusandae accusantium
-        reiciendis repudiandae. Deserunt, in laborum perspiciatis obcaecati totam quaerat, veniam eligendi maiores
-        doloremque, fuga sequi dignissimos reprehenderit optio placeat.
-        Aliquam ratione magnam iste! Dolorum, repellendus blanditiis. Natus, vitae nisi! Voluptatum ea alias neque
-        voluptate perferendis cupiditate repellendus eaque suscipit repudiandae, nisi id vero magni corporis odit
-        molestias numquam fuga!
-        Tempora nam dolores, minus, sit voluptatem dolorem molestiae, ex tempore cupiditate repellendus quae nobis in
-        incidunt? Aut sequi molestiae accusantium rem tempora doloribus quasi. Cumque consequatur quia excepturi maiores
-        in.
-        Sapiente dignissimos porro ratione eos magni explicabo velit illo, rerum expedita commodi repudiandae temporibus
-        sunt ab fugit, omnis, reprehenderit tenetur corporis? Quibusdam cum inventore modi ex harum. Nam, iste eligendi.
-        Accusamus assumenda, ipsa aperiam temporibus odit iusto recusandae omnis, reprehenderit repudiandae repellat
-        unde, sunt rerum maxime amet quae nam error vitae sit doloribus autem facere corporis. Iste aperiam vero cumque.
-    </p>
-    </div>
-</main>
-<aside id="nav-container">
-    <h3 id="tags-title">Tags</h3>
-    <ul id="tag-list">
-        <?php foreach ($currentPost['tags'] as $tag): ?>
-            <li><a href="#"><?= $tag ?></a></li>
-        <?php endforeach; ?>
-    </ul>
-    <h3 id="categories-title">Categorias</h3>
-    <ul id="categories-list">
-        <?php foreach ($categories as $category): ?>
-            <li><a href="#"><?= $category ?></a></li>
-        <?php endforeach; ?>
-    </ul>
-</aside>
+    <aside id="nav-container">
+        <h3 id="tags-title">Tags</h3>
+        <ul id="tags-list">
+            <?php foreach ($currentPost['tags'] as $tag): ?>
+                <li><a href="#"><?= $tag ?></a></li>
+            <?php endforeach; ?>
+        </ul>
+        <h3 id="categories-title">Categorias</h3>
+        <ul id="categories-list">
+            <?php foreach ($categories as $category): ?>
+                <li><a href="#"><?= $category ?></a></li>
+            <?php endforeach; ?>
+        </ul>
+    </aside>
 </main>
 <?php
 include_once("template/footer.php")
